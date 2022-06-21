4d36b8f67240279a45e9aa49e08c1b0f3543b5ee
Ramon Vinicius Marqueviski <68443821+RamonMarqueviski@users.noreply.github.com>
2022-06-21 09:23:01 -0300

    first commit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const routes = require('./routes');
-
+const cors = require('cors');
 // require('./database');
 require("dotenv").config();
 const port = process.env.PORT || 3333;
 const app = express();
 
 app.use(express.json());
-
+app.use(cors);
 app.use(routes);
 
 app.listen(port, function(){
